c7a2a0ad49732d0307b0d89cd1f4c426dac2648a
Samuel Banya <sbanya@fastmail.com>
2022-12-20 16:59:10 -0600

    Adding AddPartyForm component

---
+++ client/src/components/party/AddPartyForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function AddPartyForm({ onAddParty }) {
+    const [createPartyFormData, setCreatePartyFormData] = useState({
+        name: "",
+        start_time: "",
+        end_time: ""
+    });
+
+    const handleCreatePartyChange = (e) => {
+        setCreatePartyFormData({...createPartyFormData, [e.target.name]: e.target.value})
+    };
+
+    const handleCreatePartyFormSubmit = (e) => {
+        e.preventDefault();
+        fetch("/parties", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({ "name": createPartyFormData["name"], "start_time": createPartyFormData["start_time"], "end_time": createPartyFormData["end_time"] }),
+        })
+        .then((response) => response.json())
+        .then((newParty) => onAddParty(newParty));
+    }
+
+    return (
+        <div>
+            <h2>Add New Party</h2>
+            <form onSubmit={handleCreatePartyFormSubmit}>
+                <label htmlFor="name">Name of Party:</label>
+                <br />
+                <input onChange={handleCreatePartyChange} type="text" id="name" name="name"/>
+                <br />
+                <label htmlFor="start_time">Start Time of Party:</label>
+                <br />
+                <input onChange={handleCreatePartyChange}  type="text" id="start_time" name="start_time"/>
+                <br />
+                <label htmlFor="end_time">End Time of Party:</label>
+                <br />
+                <input onChange={handleCreatePartyChange} type="text" id="end_time" name="end_time"/>
+                <br />
+                <input type="submit"/>
+            </form>
+        </div>
+    )
+
+}
+
+export default AddPartyForm;
