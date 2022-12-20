1b12c3d0ef33a52399d897db5c51eaf49d1f3548
Samuel Banya <sbanya@fastmail.com>
2022-12-20 16:48:04 -0600

    Adding changes to App parent component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,37 +1,164 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Login from "../pages/Login";
-import RecipeList from "../pages/RecipeList";
-import NewRecipe from "../pages/NewRecipe";
+import About from "./About";
+import Party from "./party/Party";
+import Item from "./item/Item";
+import Summary from "./party/Summary";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [parties, setParties] = useState([]);
+  const [chosenParty, setChosenParty] = useState({});
+  const [partyIndex, setPartyIndex] = useState("");
+  const [itemOptions, setItemOptions] = useState([]);
+  const [itemId, setItemId] = useState("");
+  const [itemIndex, setItemIndex] = useState("");
 
   useEffect(() => {
     // auto-login
-    fetch("/api/me").then((r) => {
+    fetch("/me").then((r) => {
       if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
+        r.json()
+        .then((user) => {
+          setUser(user);
+        })
+    }
+  });
   }, []);
 
+  useEffect(() => {
+    if (chosenParty) {
+      if (chosenParty.items) {
+        let itemOptions = chosenParty.items.map((item) => {
+            return (
+                <option key={item.id} value={item.name}>{item.name}</option>
+            )
+        });
+
+        setItemOptions(itemOptions);
+      }
+    }
+  }, [chosenParty]);
+
   if (!user) return <Login onLogin={setUser} />;
+
+  function handleFetchParties(fetchedParties) {
+    setParties(fetchedParties)
+  }
+
+  function handleAddParty(newParty) {
+    const updatedPartiesArray = [...parties, newParty];
+    setParties(updatedPartiesArray);
+  }
+
+  function handleEditParty(editedParty) {
+    setParties((parties) => 
+      parties.map((party) => {
+        return party.id === editedParty.id ? editedParty : party;
+      })
+    );
+  }
+
+  function handleDeleteParty(deletedParty) {
+    setParties((parties) =>
+      parties.filter((party) => party.id !== deletedParty.id)
+    );
+  }
+
+  function handleChooseParty(e) {
+    const match = parties.find(item => item.name == e.target.value);
+
+    setChosenParty(match);
+
+    let index = parties.map(party => party.name).indexOf(e.target.value)
+
+    setPartyIndex(index);
+  }
+
+  function handleAddItem(newItem) {
+    parties.map((party) => {
+      if (party.id == chosenParty.id) {
+        const updatedItemsArray = [...party.items, newItem];
+
+        let itemOptions = updatedItemsArray.map((item) => {
+            return (
+                <option key={item.id} value={item.name}>{item.name}</option>
+            )
+        });
+
+        setItemOptions(itemOptions);
+        let tempArray = [...parties];
+        tempArray[partyIndex].items.push(newItem);
+        setParties(tempArray) ;
+      } 
+      else {
+        console.log("Match not found within 'handleAddNewItem!");
+      }});
+  }
+
+  function handleChangeItemInfo(chosenItemId, chosenItemIndex) {
+    setItemId(chosenItemId);
+    setItemIndex(chosenItemIndex);
+  }
+
+  function handleEditItem(editedItem) {
+    let tempArray = [...parties];
+    tempArray[partyIndex].items[itemIndex] = editedItem;
+    setParties(tempArray);
+
+    let itemOptions = chosenParty.items.map((item) => {
+      return (
+          <option key={item.id} value={item.name}>{item.name}</option>
+      )
+    });
+
+    setItemOptions(itemOptions);
+  }
+
+  function handleDeleteItem(response, deletedItemId) {
+    let tempArray = [...parties];
+    tempArray[partyIndex].items.splice(itemIndex, 1)
+    setParties(tempArray);
+
+    let filteredItemOptions = chosenParty.items.map((item) => {
+        return (
+            <option key={item.id} value={item.name}>{item.name}</option>
+        )
+    });
+
+    setItemOptions(filteredItemOptions);
+  }
 
   return (
     <>
       <NavBar user={user} setUser={setUser} />
-      <main>
-        <Switch>
-          <Route path="/new">
-            <NewRecipe user={user} />
-          </Route>
-          <Route path="/">
-            <RecipeList />
-          </Route>
-        </Switch>
-      </main>
+      <Routes>
+        <Route 
+          path="/about" 
+          element={<About user={user}/>} 
+        />
+        <Route 
+          path="/parties" 
+          element={<Party 
+            parties={parties} onFetchParties={handleFetchParties} onChooseParty={handleChooseParty} chosenParty={chosenParty}
+            onAddParty={handleAddParty} onEditParty={handleEditParty} onDeleteParty={handleDeleteParty} 
+          />}
+        />
+        <Route 
+          path="/items" 
+          element={<Item 
+            parties={parties} onChooseParty={handleChooseParty} chosenParty={chosenParty} onFetchParties={handleFetchParties}
+            onAddItem={handleAddItem} itemOptions={itemOptions} setItemOptions={setItemOptions} itemId={itemId} setItemId={setItemId} onChangeItemInfo={handleChangeItemInfo}
+            onEditItem={handleEditItem} onDeleteItem={handleDeleteItem} 
+          />}
+        />
+        <Route 
+          path="/summary" 
+          element={<Summary parties={parties} onFetchParties={handleFetchParties} />}
+        />
+      </Routes>
     </>
   );
 }
