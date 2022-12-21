a088f7365edc92a09ae53b9ac2634c1958b87a28
Samuel Banya <sbanya@fastmail.com>
2022-12-20 20:20:18 -0600

    Adding latest notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
     skip_before_action :authorize, only: :create
 
     def create
+        # TODO: 
+        # Build the application locally, and put a byebug here so that we can figure out why 
+        # the 'Uncaught (in promise) SyntaxError: Unexpected end of JSON input' error is occurring
         user = User.create!(user_params)
         session[:user_id] = user.id
         render json: user, status: :created
