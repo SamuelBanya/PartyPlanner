c37d38865cc152d076c63c244dd11e8f58d2fa46
Samuel Banya <sbanya@fastmail.com>
2022-12-20 18:15:30 -0600

    Adding revision to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
+import {  BrowserRouter, Routes, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
