5a4c713e83574ecc37c7383d76a869ec0bd1f219
Samuel Banya <sbanya@fastmail.com>
2023-01-13 17:04:09 -0600

    Adding change to include Google Maps on export for build script

--- bin/party-build.sh
+++ bin/party-build.sh
@@ -13,3 +13,8 @@ bundle exec rake db:migrate
 # NOTE:
 # Commenting out seed data since we already seeded the database locally:
 # bundle exec rake db:seed
+
+# Adding Google Maps API key specific environment variable:
+# From the Render.com docs page:
+# https://render.com/docs/deploy-create-react-app
+export REACT_APP_GOOGLE_API_KEY=$REACT_APP_GOOGLE_API_KEY
