31b281bfab6aeb97b5039c6738164e6654826d52
Samuel Banya <sbanya@fastmail.com>
2023-01-13 17:49:00 -0600

    Reverting back to original environment variable attempt

--- bin/party-build.sh
+++ bin/party-build.sh
@@ -17,7 +17,8 @@ bundle exec rake db:migrate
 # Adding Google Maps API key specific environment variable:
 # From the Render.com docs page:
 # https://render.com/docs/deploy-create-react-app
-# PREVIOUS ATTEMPT:
-# export REACT_APP_GOOGLE_API_KEY=$REACT_APP_GOOGLE_API_KEY
+# FIRST ATTEMPT:
+export REACT_APP_GOOGLE_API_KEY=$REACT_APP_GOOGLE_API_KEY
 # Trying to shorten the name of the variable name so that Render doesn't be so fussy with the API key itself since it seems like they shorten this for some reason:
-export REACT_APP_GOOGLE_API_KEY=$GOOGLE_API_KEY
+# SECOND ATTEMPT:
+# export REACT_APP_GOOGLE_API_KEY=$GOOGLE_API_KEY
