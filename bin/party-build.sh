bbb6c051c505102a2d82de8140ee5a646c9eb6dc
Samuel Banya <sbanya@fastmail.com>
2023-01-13 17:30:09 -0600

    Trying to utilize shorter syntax to see if Render isn't so fussy this time

--- bin/party-build.sh
+++ bin/party-build.sh
@@ -17,4 +17,7 @@ bundle exec rake db:migrate
 # Adding Google Maps API key specific environment variable:
 # From the Render.com docs page:
 # https://render.com/docs/deploy-create-react-app
-export REACT_APP_GOOGLE_API_KEY=$REACT_APP_GOOGLE_API_KEY
+# PREVIOUS ATTEMPT:
+# export REACT_APP_GOOGLE_API_KEY=$REACT_APP_GOOGLE_API_KEY
+# Trying to shorten the name of the variable name so that Render doesn't be so fussy with the API key itself since it seems like they shorten this for some reason:
+export REACT_APP_GOOGLE_API_KEY=$GOOGLE_API_KEY
