0d04ec36fb039814e0d8b496a3bf27114448ea34
Samuel Banya <sbanya@fastmail.com>
2023-03-13 16:31:51 -0500

    Adding change to roadmap item since it was already completed

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -12,46 +12,43 @@ import { HelloContext } from "./context/HelloContext";
 // https://www.pragimtech.com/blog/reactjs/usecontext-hook-in-react/
 
 function About({ user }) {
-    const value = useContext(HelloContext);
-    return (
-        <div>
-            <h2>About Page</h2>
-            <h3>{value}, {user.username}!</h3>
-            <p>
-                This app is a Full Stack web application made with React, Ruby On Rails, and PostgresSQL that allows a user to plan a party with their friends!
-            </p>
-            <h2>Current Functions</h2>
-            <ul>
-                <li>
-                    Login page where a user can create an account, and login to the site
-                </li>
-                <li>
-                    The ability to create a party with a start and end time
-                </li>
-                <li>
-                    The ability to add new items to an existing party as well as edit or delete the items
-                </li>
-                <li>
-                    The ability to create, edit, and delete a item
-                </li>
-                <li>
-                    The ability to display a summary page to view all users' parties
-                </li>
-            </ul>
-            <h2>Roadmap Items</h2>
-            <ul>
-                <li>
-                    Timestamp selection to easily pick timestamp for each party   
-                </li>
-                <li>
-                    Ability to utilize a map library from NPM for the 'Summary' page
-                </li>
-                <li>
-                    Use of MaterialUI throughout the project itself 
-                </li>
-            </ul>
-        </div>
-    )
+  const value = useContext(HelloContext);
+  return (
+    <div>
+    <h2>About Page</h2>
+    <h3>{value}, {user.username}!</h3>
+    <p>
+    This app is a Full Stack web application made with React, Ruby On Rails, and PostgresSQL that allows a user to plan a party with their friends!
+    </p>
+    <h2>Current Functions</h2>
+    <ul>
+    <li>
+    Login page where a user can create an account, and login to the site
+    </li>
+    <li>
+    The ability to create a party with a start and end time
+    </li>
+    <li>
+    The ability to add new items to an existing party as well as edit or delete the items
+    </li>
+    <li>
+    The ability to create, edit, and delete a item
+    </li>
+    <li>
+    The ability to display a summary page to view all user parties
+    </li>
+    <li>
+    Ability to utilize a map library from NPM for the 'Summary' page
+    </li>
+    </ul>
+    <h2>Roadmap Items</h2>
+    <ul>
+    <li>
+    Timestamp selection to easily pick timestamp for each party
+    </li>
+    </ul>
+    </div>
+  )
 }
 
 export default About;
